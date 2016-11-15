6dbac38106076c687db86130461f7da19bd982e3
Václav <vaclav.volhejn@gmail.com>
2016-11-15 08:06:06 +0100

    Rychlost animace (rychlejší na začátku, pomalejší na konci

--- js/animation.js
+++ js/animation.js
@@ -12,10 +12,12 @@ function isWhite (x, y) {
   return data[3] === 0 || (data[0] + data[1] + data[2]) >= 3 * 250
 }
 
-var angle = 0
+var startTime = Date.now()
 var update = function () {
-  angle = Math.random() * 2 * Math.PI
-  // angle += 2
+  var angle = Math.random() * 2 * Math.PI
+  var t = Date.now() - startTime
+  var hitboxR = Math.log(t) / 12
+  hitboxR *= PARTICLE_HITBOX_RADIUS
   var x = canvas.width / 2 - Math.cos(angle) * canvas.width * 0.7
   var y = canvas.height / 2 - Math.sin(angle) * canvas.width * 0.7
   x += (Math.random() - 0.5) * 20
@@ -29,8 +31,8 @@ var update = function () {
     }
     var stop = false
     for (var i = 0; i < SAMPLES; i++) {
-      var x1 = x + Math.cos((Math.PI * 2 / SAMPLES) * i) * PARTICLE_HITBOX_RADIUS * canvas.width
-      var y1 = y + Math.sin((Math.PI * 2 / SAMPLES) * i) * PARTICLE_HITBOX_RADIUS * canvas.width
+      var x1 = x + Math.cos((Math.PI * 2 / SAMPLES) * i) * hitboxR * canvas.width
+      var y1 = y + Math.sin((Math.PI * 2 / SAMPLES) * i) * hitboxR * canvas.width
       if (!isWhite(x1, y1)) stop = true
     }
     if (stop) {
